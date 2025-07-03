dc7800b5a813a8ebee4155167b306f5b9d431f01
Francisco Scagliarini <fscagliarini@incucai.gov.ar>
2025-07-03 16:08:43 -0300

    componente buscar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
+import  Buscar  from './components/Buscar.jsx'
 
 const App = () => {
+  const [buscarTerm, setBuscarTerm] = useState('')
   return(
     <main>
       <div>
         <div className='pattern'/>
         <div className='wrapper'>
           <header>
+            <img src="./pelis-arg.png" alt="heroe papaa" />
             <h1>Encontra las <span className='text-gradient'>peliculas</span> Argentinas en The Movie Database </h1>
           </header>
+
+          <Buscar/>
         </div>
 
       </div>
---
+++ src/components/Buscar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Buscar = () => {
+    return(
+        <div className='text-white text-3xl'> Buscar 🔍 </div>
+    )
+}
+
+export default Buscar
